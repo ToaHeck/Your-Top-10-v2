87cc27fc5d8c4db2a5161cb0cd31475b73523512
ToaHeck <toaheck1@gmail.com>
2025-04-17 12:30:57 -0700

    renamed environment variables

--- netlify/functions/spotify-auth.js
+++ netlify/functions/spotify-auth.js
@@ -1,16 +1,19 @@
+// netlify/functions/spotify-auth.js
+
 export async function handler(event, context) {
     const clientId = process.env.SPOTIFY_CLIENT_ID;
     const redirectUri = process.env.SPOTIFY_REDIRECT_URI_NETLIFY;
-    const scopes = "user-read-private user-read-email user-top-read";
   
     const { code_challenge } = JSON.parse(event.body || '{}');
-    
+  
     if (!code_challenge) {
       return {
         statusCode: 400,
-        body: "Missing code_challenge"
+        body: "Missing code_challenge",
       };
     }
+  
+    const scopes = "user-read-private user-read-email user-top-read";
   
     const authUrl = `https://accounts.spotify.com/authorize?` +
       `client_id=${clientId}&` +
@@ -22,7 +25,7 @@ export async function handler(event, context) {
   
     return {
       statusCode: 200,
-      body: JSON.stringify({ authUrl })
+      body: JSON.stringify({ authUrl }),
     };
   }
   
